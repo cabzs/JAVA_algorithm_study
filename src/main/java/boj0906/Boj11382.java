6c53c38c7a64d990cbe4c96806ef7badd0858df2
sunjung <sunj20037@gmail.com>
2022-09-06 01:28:32 +0900

    0906_11382

---
+++ src/main/java/boj0906/Boj11382.java
@@ -0,0 +1,25 @@
+package boj0906;
+
+import java.util.Scanner;
+
+public class Boj11382 {
+
+	public static void main(String[] args) {
+		//첫 번째 줄에 A, B, C가 공백을 사이에 두고 주어진다.
+		//A+B+C
+		
+		Scanner sc = new Scanner(System.in);
+//		int a = sc.nextInt();
+//		int b = sc.nextInt();
+//		int c = sc.nextInt();
+		
+		Long a = sc.nextLong();
+		Long b = sc.nextLong();
+		Long c = sc.nextLong();
+		
+		
+		System.out.println(a+b+c);
+		
+	}
+
+}
